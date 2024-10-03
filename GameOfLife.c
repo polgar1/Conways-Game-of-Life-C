652e76487fddcbada42921bfd2f2d51d8cdf5125
polgar1 <punga9111@gmail.com>
2024-10-03 15:38:13 +0300

    Add CalculateNextGeneration function

--- GameOfLife.c
+++ GameOfLife.c
@@ -37,3 +37,49 @@ void print(int grid[70][70])
     }
     return;
 }
+
+void nextGeneration(int grid[70][70])
+{
+    int tempGrid[70][70];
+    for (int i = 0; i < 70; i++)
+    {
+        for (int j = 0; j < 70; j++)
+        {
+            int numOfNeighbors = 0;
+            if (i > 0 && j > 0 && grid[i-1][j-1] == 1) numOfNeighbors++;
+            if (i > 0 && grid[i-1][j] == 1) numOfNeighbors++;
+            if (i > 0 && j < 69 && grid[i-1][j+1] == 1) numOfNeighbors++;
+            if (j > 0 && grid[i][j-1] == 1) numOfNeighbors++;
+            if (j < 69 && grid[i][j+1] == 1) numOfNeighbors++;
+            if (i < 69 && j > 0 && grid[i+1][j-1] == 1) numOfNeighbors++;
+            if (i < 69 && grid[i+1][j] == 1) numOfNeighbors++;
+            if (i < 69 && j < 69 && grid[i+1][j+1] == 1) numOfNeighbors++;
+
+            if (grid[i][j] == 1)
+            {
+                if (numOfNeighbors == 2 || numOfNeighbors == 3)
+                {
+                    tempGrid[i][j] = 1;
+                }
+                else
+                {
+                    tempGrid[i][j] = 0;
+                }
+            }
+            else if (numOfNeighbors == 3)
+            {
+                tempGrid[i][j] = 1;
+            }
+            
+        }
+    }
+    
+    for (int i = 0; i < 70; i++)
+    {
+        for (int j = 0; j < 70; j++)
+        {
+            grid[i][j] = tempGrid[i][j];
+        }
+    }
+
+}
